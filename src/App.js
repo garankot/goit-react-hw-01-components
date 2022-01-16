2300b9f9155b293a9f57e7bfda06923117819d9d
Taras Garan'ko <garanko.t@gmail.com>
2022-01-17 00:34:33 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 import PropTypes from 'prop-types';
+import Profile from './component/Profile/Profile';
+
+import user from './social-profile/user.json';
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+};
+
+export default App;
---
+++ src/component/Profile/Profile.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+
+import {
+  ProfileCard,
+  AvatarWrapper,
+  Avatar,
+  UserData,
+  Stats,
+  StatsItem,
+  UserName,
+  StatsLabel,
+} from '../Profile/Profile.styled';
+
+function Profile({ username, tag, location, avatar, stats }) {
+  return (
+    <ProfileCard>
+      <AvatarWrapper>
+        <Avatar src={avatar} alt="User avatar" />
+      </AvatarWrapper>
+      <UserData>
+        <UserName>{username}</UserName>
+        <p>{`@${tag}`}</p>
+        <p>{location}</p>
+      </UserData>
+      <Stats>
+        <StatsItem>
+          <StatsLabel>Followers</StatsLabel>
+          <span>{stats.followers}</span>
+        </StatsItem>
+        <StatsItem>
+          <StatsLabel>Views</StatsLabel>
+          <span>{stats.views}</span>
+        </StatsItem>
+        <StatsItem>
+          <StatsLabel>Likes</StatsLabel>
+          <span>{stats.likes}</span>
+        </StatsItem>
+      </Stats>
+    </ProfileCard>
+  );
+}
+
+Profile.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: (PropTypes.objectOf = {
+    [PropTypes.string]: PropTypes.string,
+    [PropTypes.string]: PropTypes.number,
+  }),
+};
+export default Profile;
